a6b364a4ed663fc7f15524dccee3da927cb25219
Rafael Matias <rafamatias102@gmail.com>
2026-01-09 12:00:40 +0000

    add healthCheck api endpoint add sampleTask api endpoint add newTask api endpoint fix issue where api was not listening on any port fix cors related conectivity issue between FE and BE add Task class add frontend api call functions and related DOM constant loading, related event listners and handler functions

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-import { database } from "./database.js"
-
 import express from "express"
-// const express = require('express')
+import cors from "cors"
+
+import { Task } from "./models.js"
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(cors());
+app.use(express.json())
+
+// sample DB object (sample data)
+const sampleTask = {
+  taskName: "Do the dishes",
+  taskCategory: "House Shores",
+  deadline: "today",
+  priotiy: 10
+}
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
 })
 
-app.get('/tasks', (req, res) => {
-    if (database.tasks.length == 0) {
-        console.log("if entered!")
-        res.send('There are no tasks yet')
-    } 
-    else {
-        console.log("else entered!")
-        res.send(database.tasks)
-    }
+app.get('/sampleTask', (req, res) => {
+  res.send(JSON.stringify(sampleTask));
 })
 
-app.post('/task', (req, res) => {
-    const taskAmount = database.tasks.length;
-    const newTaskNumber = taskAmount + 1;
-    database.tasks.push(`task item ${newTaskNumber}`);
-    res.json(database.tasks); // send proper JSON back
-});
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.post('/newTask', (req, res) => {
+  console.log(req.body)
+  res.send(JSON.stringify("task received!"))
 })
+
+app.listen(port, () => console.log(`Server listening on ${port}`));
+
+
---
+++ models.js
@@ -0,0 +1,8 @@
+export class Task {
+    constructor(name, category, deadline, priority) {
+        this.name = name,
+        this.category = category,
+        this.deadline = deadline,
+        this.priority = priority
+    }
+}
